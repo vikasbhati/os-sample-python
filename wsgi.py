0bdb0399758d6978b7f81782e5671f7bab713f5e
vikasbhati <31238674+vikasbhati@users.noreply.github.com>
2018-06-05 19:40:02 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -15,7 +15,7 @@ def hey(name):
     return render_template(
         'test.html',name=name)
 
-@application.route('/login', method=['POST'])
+@application.route('/login', methods=['POST'])
 def do_admin_login():
     if not session.get('logged_in'):
         session['logged_in'] = True
