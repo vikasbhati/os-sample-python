1355dd877e41c720bf698e93fe21d493598546e8
vikasbhati <31238674+vikasbhati@users.noreply.github.com>
2018-06-05 18:31:25 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,14 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+
 application = Flask(__name__)
 
-@application.route("/")
-def hello():
-    return "Hello World!"
+@application.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
 
 @application.route("/hey/<string:name>")
 def hey(name):
     return render_template(
         'test.html',name=name)
 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+
 if __name__ == "__main__":
+    application.secret_key = os.urandom(12)
     application.run()
