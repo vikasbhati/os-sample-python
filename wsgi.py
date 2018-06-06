3028cc1eefe1fe4a1cb9a6ec35baf1ce16b8bfa3
vikasbhati <31238674+vikasbhati@users.noreply.github.com>
2018-06-06 11:55:41 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -12,8 +12,17 @@ def hello():
 
 @application.route("/hey/<string:name>")
 def hey(name):
+    quotes = [ "'If people do not believe that mathematics is simple, it is only because they do not realize how complicated life is.' -- John Louis von Neumann ",
+               "'Computer science is no more about computers than astronomy is about telescopes' --  Edsger Dijkstra ",
+               "'To understand recursion you must first understand recursion..' -- Unknown",
+               "'You look at things that are and ask, why? I dream of things that never were and ask, why not?' -- Unknown",
+               "'Mathematics is the key and door to the sciences.' -- Galileo Galilei",
+               "'Not everyone will understand your journey. Thats fine. Its not their journey to make sense of. Its yours.' -- Unknown"  ]
+    randomNumber = randint(0,len(quotes)-1) 
+    quote = quotes[randomNumber]
+    
     return render_template(
-        'test.html',name=name)
+        'test.html', **locals() )
 
 @application.route('/login', methods=['POST'])
 def do_admin_login():
